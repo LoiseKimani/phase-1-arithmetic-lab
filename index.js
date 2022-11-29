e4c7bb974176c134d763d4ca57b09ef22dd19351
Loise <loise.kimani@student.moringaschool.com>
2022-11-29 08:48:42 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-
 // Write your code here
+
+//test multiply
+
+    let num1=2
+    let num2=31
+
+   let  multiply = num1 * num2
+  
+
+//test random
+
+    let random = Math.floor(Math.random()*11)+1
+
+//test mod
+
+    let num3 =92
+    let num4 =22
+     let mod = num3 % num4
+
+
+//test max  
+
+    let max = Math.max(3,5,13,16,20)
+
+
+
+console.log(multiply(4,5))
